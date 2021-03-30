14f8a222f1848aa151f3a242fa918ed4eb95d63a
Memagog <55204188+Memagog@users.noreply.github.com>
2021-03-31 01:26:47 +0300

    Update Main.java

--- src/com/company/dua_lab/Main.java
+++ src/com/company/dua_lab/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String filename = args[0];
         readAndOutputResult(filename);
+
     }
 
     private static void readAndOutputResult(String fileName) throws IOException {
@@ -17,7 +18,8 @@ public class Main {
         String[] s = new String[2];
         String line;
         ArrayList<String> list = new ArrayList<>();
-        ArrayList<String> solution = new ArrayList<>();
+        ArrayList<String> test = new ArrayList<>();
+        ArrayList<String> result = new ArrayList<>();
         while ((line = br.readLine()) != null) {
             s = line.split(" ");
             for (String i : s) {
@@ -26,17 +28,22 @@ public class Main {
         }
         br.close();
         fr.close();
-        solution.addAll(bestsTrip(list));
-        Collections.sort(solution, Collections.reverseOrder(String.CASE_INSENSITIVE_ORDER));
+        test.addAll(bestsTrip(list));
+        Collections.sort(test);
         String p = "";
-        for (int i = 0; i < solution.size(); i++) {
-            if(solution.get(i).contains("Grotty")){
-                solution.add(i,p);
-                break;
+        for (int i = 0; i < test.size(); i++) {
+            if(!test.get(i).contains("Grotty")){
+                result.add(test.get(i));
+            }
+        }
+        result.add(p);
+        for(String j: test){
+            if(j.contains("Grotty")){
+                result.add(j);
             }
         }
         FileWriter writer = new FileWriter("output.txt");
-        for(String str : solution){
+        for(String str : result){
             writer.write(str +"\n");
         }
         writer.close();
@@ -50,19 +57,24 @@ public class Main {
         for (int i = 0; i < a.size() - 1; i++) {
             if (i == 1 || i % 3 == 1) {
                 int x = TripTime.resultTime(a.get(i),a.get(i+1));
-                if(x<60)
+                if(x<60){
                     result.add(a.get(i-1)+" " + a.get(i) + " "+ a.get(i+1));
-
+                }
             }
 
         }
+
         for (int i = 0; i < a.size() - 1; i++) {
+
             if (i == 1 || i % 3 == 1) {
+
+
                 for(int j = i+3;j<a.size() - 1;){
                     boolean tr= a.get(i).equals(a.get(j));
                     boolean td = a.get(i+1).equals(a.get(j+1));
                     if(tr&&td){
                         if(a.get(i-1).equals("Grotty")){
+
                             result.remove(a.get(i-1)+" " + a.get(i) + " "+ a.get(i+1));
                         }
                         else if(a.get(j-1).equals("Grotty")){
@@ -79,8 +91,8 @@ public class Main {
 
                     j+=3;
                 }
+              }
             }
-        }
 
         return result;
     }
